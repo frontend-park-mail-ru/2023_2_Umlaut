72ac8441f3bfb935e0f7d748a2002aa336597ec5
Polina <stepolant@gmail.com>
2023-09-26 19:19:19 +0300

    static layout of feed page

---
+++ public/feed.js
@@ -0,0 +1,4 @@
+const rootElement = document.querySelector('#root');
+const userFormElement = document.createElement('div');
+userFormElement.textContent("qwertyuio");
+rootElement.appendChild(userFormElement);
