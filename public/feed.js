d776e6d254e1f03c3ec092c41225f8f54271e02d
Polina <stepolant@gmail.com>
2023-09-26 23:41:25 +0300

    fixed some bugs

--- public/feed.js
+++
@@ -1,4 +0,0 @@
-const rootElement = document.querySelector('#root');
-const userFormElement = document.createElement('div');
-userFormElement.textContent("qwertyuio");
-rootElement.appendChild(userFormElement);
