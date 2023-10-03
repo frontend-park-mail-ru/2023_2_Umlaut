f51d44d4d08ea740ce0bda1babc4954f0c08d82c
Dmitriy Ostrometskiy <dima-o-2003@mail.ru>
2023-10-03 03:56:48 +0300

    ajax changes

--- public/modules/ajax.js
+++ public/modules/ajax.js
@@ -1,11 +1,3 @@
-export const URLS = {
-  login: "/auth/login",
-  logout: "/auth/logout",
-  singup: "/auth/signup",
-  feed: "/feed",
-};
-
-export const BACKEND_URL = "http://localhost:8000";
 
 export class Ajax {
   static get(params = {}) {
@@ -25,37 +17,10 @@ export class Ajax {
           console.error(error); // ошибка отправки
         }
       )
-      .then((parsedJson) => {
+      .then((body) => {
         return {
           status,
-          parsedJson,
-        };
-      });
-  }
-
-  static getNoBody(params = {}) {
-    let status;
-
-    return fetch(params.url, {
-      method: "GET",
-      mode: "cors",
-      credentials: "include",
-    })
-      .then(
-        (response) => {
-          status = response.status
-          return {
-            status
-          }
-        },
-        (error) => {
-          console.error(error); // ошибка отправки
-        }
-      )
-      .then((parsedJson) => {
-        return {
-          status,
-          parsedJson,
+          body,
         };
       });
   }
@@ -114,34 +79,4 @@ export class Ajax {
       );
   }
 
-  static async postAsync(url = "", data = {}) {
-    const response = await fetch(url, {
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const parsedJson = await response.json();
-    return {
-      status: response.status,
-      parsedJson,
-    };
-  }
-
-  static async getAsync(params = {}) {
-    const response = await fetch(params.url, {
-      method: "GET",
-      mode: "cors",
-      credentials: "include",
-    });
-    const parsedJson = await response.json();
-
-    return {
-      status: response.status,
-      parsedJson,
-    };
-  }
 }
