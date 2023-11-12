4ba703e06f1333018779140f35db026fc15bf0c3
Polina <stepolant@gmail.com>
2023-11-13 01:15:24 +0300

    Revert "lint"
    This reverts commit dbd96d3052f3724948b137248731206cd936d271.

--- public/components/Messenger/MessengerModel.js
+++ public/components/Messenger/MessengerModel.js
@@ -1,41 +1,44 @@
-import { MESSENGER_EVENTS } from "../../lib/constansts.js";
-import { Api } from "../../lib/api.js";
+import {MESSENGER_EVENTS} from '../../lib/constansts.js';
+import {Api} from '../../lib/api.js';
 
 export class MessengerModel {
-  constructor(eventBus) {
-    this.eventBus = eventBus;
-    this.eventBus.on(MESSENGER_EVENTS.GET_DIALOGS, this.getDialogs.bind(this));
-    this.eventBus.on(MESSENGER_EVENTS.GET_PAIRS, this.getPairs.bind(this));
-  }
-
-  getDialogs() {
-    this.eventBus.emit(MESSENGER_EVENTS.DIALOGS_READY, null);
-  }
-
-  getPairs() {
-    Api.getPairs().then((response) => {
-      if (response.status === 200) {
-        const dialogs = response.payload;
-        this.addPhotos(dialogs).then((dialogs) =>
-          this.eventBus.emit(MESSENGER_EVENTS.PAIRS_READY, dialogs)
-        );
-      } else if (response.status === 401) {
-        this.eventBus.emit(MESSENGER_EVENTS.UNAUTH);
-      }
-    });
-  }
-
-  addPhotos(dialogs) {
-    if (dialogs) {
-      dialogs.forEach((element) => {
-        Api.getUserPhotoUrl(element.user2_id).then((image) => {
-          element.photo = image;
-          if (image === undefined) {
-            element.photo = "/pics/avatar.png";
-          }
-        });
-      });
+    constructor(eventBus) {
+        this.eventBus = eventBus;
+        this.eventBus.on(MESSENGER_EVENTS.GET_DIALOGS, this.getDialogs.bind(this));
+        this.eventBus.on(MESSENGER_EVENTS.GET_PAIRS, this.getPairs.bind(this));
     }
-    return Promise.resolve(dialogs);
-  }
+
+    getDialogs() {
+        this.eventBus.emit(MESSENGER_EVENTS.DIALOGS_READY, null);
+    }
+
+    getPairs() {
+        Api.getPairs().then(
+            (response) => {
+                if ( response.status === 200) {
+                    const dialogs = response.payload;
+                    this.addPhotos(dialogs).then((dialogs)=> this.eventBus.emit(MESSENGER_EVENTS.PAIRS_READY, dialogs));
+                    
+                } else if ( response.status === 401 ) {
+                    this.eventBus.emit(MESSENGER_EVENTS.UNAUTH);
+                }
+            },
+        );
+    }
+
+    addPhotos(dialogs){
+        if(dialogs){
+            dialogs.forEach((element) => {
+                Api.getUserPhotoUrl(element.user2_id).then(
+                    (image) =>{
+                        element.photo = image;
+                        if (image === undefined) {
+                            element.photo = '/pics/avatar.png';
+                        }
+                    },
+                );
+            });
+        }
+        return Promise.resolve(dialogs);
+    }
 }
--- public/components/Messenger/MessengerView.js
+++ public/components/Messenger/MessengerView.js
@@ -1,81 +1,74 @@
-import { BaseView } from "../BaseView.js";
-import { MESSENGER_EVENTS } from "../../lib/constansts.js";
+import {BaseView} from '../BaseView.js';
+import {MESSENGER_EVENTS} from '../../lib/constansts.js';
 
 export class MessengerView extends BaseView {
-  constructor(root, eventBus) {
-    super(root, eventBus, require("./Messenger.hbs"));
-    this.eventBus.on(
-      MESSENGER_EVENTS.DIALOGS_READY,
-      this.addDialogs.bind(this)
-    );
-    this.eventBus.on(MESSENGER_EVENTS.PAIRS_READY, this.addPairs.bind(this));
-    this.dialogPreviewTemplate = require("./DialogPreview.hbs");
-    this.dialogWindow = null;
-    this.dialogListView = null;
-    this.dialogList = null;
-  }
-
-  render(data) {
-    super.render(data);
-
-    this.dialogList = [];
-    this.dialogListView = document.getElementById("dialog-list");
-    this.dialogWindow = document.getElementById("dialog-window");
-
-    this.showDialogs = document.getElementById("show_dialogs");
-    this.showPairs = document.getElementById("show_pairs");
-
-    this.showDialogs.addEventListener("click", () =>
-      this.eventBus.emit(MESSENGER_EVENTS.GET_DIALOGS)
-    );
-    this.showPairs.addEventListener("click", () =>
-      this.eventBus.emit(MESSENGER_EVENTS.GET_PAIRS)
-    );
-
-    if (!data) {
-      this.eventBus.emit(MESSENGER_EVENTS.GET_DIALOGS);
+    constructor(root, eventBus) {
+        super(root, eventBus, require('./Messenger.hbs'));
+        this.eventBus.on(MESSENGER_EVENTS.DIALOGS_READY, this.addDialogs.bind(this));
+        this.eventBus.on(MESSENGER_EVENTS.PAIRS_READY, this.addPairs.bind(this));
+        this.dialogPreviewTemplate = require('./DialogPreview.hbs');
+        this.dialogWindow = null;
+        this.dialogListView = null;
+        this.dialogList = null;
     }
-  }
 
-  addDialogs(data) {
-    if (data) {
-      this.addData(data);
-    } else {
-      this.addEmptyDialogs(require("./EmptyDialogs.hbs"));
+    render(data) {
+        super.render(data);
+
+        this.dialogList = [];
+        this.dialogListView = document.getElementById('dialog-list');
+        this.dialogWindow = document.getElementById('dialog-window');
+    
+        this.showDialogs = document.getElementById('show_dialogs');
+        this.showPairs = document.getElementById('show_pairs');
+
+        this.showDialogs.addEventListener('click', () => this.eventBus.emit(MESSENGER_EVENTS.GET_DIALOGS));
+        this.showPairs.addEventListener('click', () => this.eventBus.emit(MESSENGER_EVENTS.GET_PAIRS));
+
+        if (!data) {
+            this.eventBus.emit(MESSENGER_EVENTS.GET_DIALOGS);
+        }
     }
-  }
 
-  addPairs(data) {
-    if (data) {
-      this.addData(data);
-    } else {
-      this.addEmptyDialogs(require("./EmptyPairs.hbs"));
+    addDialogs(data) {
+        if(data) {
+            this.addData(data);
+        }else{
+            this.addEmptyDialogs(require('./EmptyDialogs.hbs'));
+        }
     }
-  }
 
-  addData(data) {
-    this.dialogList = [];
-    this.dialogListView.innerHTML = "";
-    data.forEach((dialog) => {
-      const dialogPreview = document.createElement("div");
-      dialogPreview.innerHTML = this.dialogPreviewTemplate(dialog);
-      this.dialogListView.appendChild(dialogPreview);
-      this.dialogList.push(dialogPreview);
-    });
-  }
+    addPairs(data) {
+        if(data) {
+            this.addData(data);
+        }else{
+            this.addEmptyDialogs(require('./EmptyPairs.hbs'));
+        }
+    }
 
-  addEmptyDialogs(empty) {
-    this.dialogListView.innerHTML = "";
-    const dialogPreview = document.createElement("div");
-    dialogPreview.innerHTML = empty();
-    this.dialogListView.appendChild(dialogPreview);
-    this.dialogList.push(dialogPreview);
-  }
+    addData(data){
+        this.dialogList = [];
+            this.dialogListView.innerHTML = '';
+            data.forEach((dialog) => {
+                const dialogPreview = document.createElement('div');
+                dialogPreview.innerHTML = this.dialogPreviewTemplate(dialog);
+                this.dialogListView.appendChild(dialogPreview);
+                this.dialogList.push(dialogPreview);
+            });
+    }
 
-  close() {
-    super.close();
-    this.dialogWindow = null;
-    this.dialogListView = null;
-    this.dialogList = null;
-  }
+    addEmptyDialogs(empty){
+        this.dialogListView.innerHTML = '';
+        const dialogPreview = document.createElement('div');
+        dialogPreview.innerHTML = empty();
+        this.dialogListView.appendChild(dialogPreview);
+        this.dialogList.push(dialogPreview);
+    }
+
+    close() {
+        super.close();
+        this.dialogWindow = null;
+        this.dialogListView = null;
+        this.dialogList = null;
+    }
 }
