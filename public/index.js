8f85c41e704c25d310c5034d279da2412ff36b30
Polina <stepolant@gmail.com>
2023-11-24 15:25:30 +0300

    add offline event

--- public/index.js
+++ public/index.js
@@ -43,6 +43,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
         router.go('/auth');
     });
 
+    window.addEventListener("offline", () => {
+        popup.render("Отсутсвует подключение к интернету");
+    });
+
+    window.addEventListener("online", () => {
+        popup.render("Подключение восстановлено");
+    });
+
     globalEventBus.emit(GLOBAL_EVENTS.AUTH);
 
     const auth = new AuthController(page, globalEventBus);
