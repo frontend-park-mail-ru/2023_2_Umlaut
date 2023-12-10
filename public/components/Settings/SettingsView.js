68ff3bab4c5da721b66ce8cf5dcf04eea0111473
Dmitriy Ostrometskiy <dima-o-2003@mail.ru>
2023-12-10 22:44:24 +0300

    first try

--- public/components/Settings/SettingsView.js
+++ public/components/Settings/SettingsView.js
@@ -203,7 +203,7 @@ export class SettingsView extends BaseView {
             this.showError('Почта не может содержать специальные символы');
             return false;
         }
-        if(this.form.querySelectorAll('.multiselection__selection_active').length>6){
+        if (this.form.querySelectorAll('.multiselection__selection_active').length > 6) {
             this.showError('Выберите не более 6 тегов');
             return false;
         }
