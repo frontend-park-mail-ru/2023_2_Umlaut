ced1a4cc29b828301d878b9df2963d591249598e
Dmitriy Ostrometskiy <dima-o-2003@mail.ru>
2023-12-17 17:11:02 +0300

    csat fix

--- csat/index.js
+++ csat/index.js
@@ -1,4 +1,5 @@
-import { Csat } from "./components/Welcome/Csat.js";
+import { Api } from "../public/lib/api.js";
+import { Csat, close } from "./components/Welcome/Csat.js";
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const root = document.querySelector('.csat-place'); 
@@ -7,12 +8,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
         if (message.origin !== window.location.origin) {
             return;
         }
-        if(message.data===1){
-            csat.renderAboutAll();
-        }else if(message.data===2){
-            csat.renderReccomendToFriend();
-        }else if(message.data===3){
-            csat.renderAboutFeed();
-        }
+        Api.user().then((responce)=>{
+            if(responce.status === 401)
+                close();
+            else if(responce.status === 200) {
+                if(message.data===1){
+                    csat.renderAboutAll();
+                }else if(message.data===2){
+                    csat.renderReccomendToFriend();
+                }else if(message.data===3){
+                    csat.renderAboutFeed();
+                }
+            }
+        })
     });
 });
